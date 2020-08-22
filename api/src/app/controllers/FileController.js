7dc3cce44308ce0adcb9ed194b9df78d8527dfca
mpinheiro <mpinheiro@orion.tech>
2020-08-22 19:57:58 -0300

    upload files and relationsip with user

---
+++ api/src/app/controllers/FileController.js
@@ -0,0 +1,19 @@
+import File from '../models/File';
+
+class FileController {
+  async store(req, res) {
+    try {
+      const { originalname: name, filename: path } = req.file;
+
+      const file = await File.create({
+        name,
+        path,
+      });
+      return res.json(file);
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+  }
+}
+
+export default new FileController();
