2594bfeebd0c5705be1f929a6d78a5446c1998af
mpinheiro <mpinheiro@orion.tech>
2020-08-22 17:56:17 -0300

    feat login and update user

--- api/src/app/controllers/UserController.js
+++ api/src/app/controllers/UserController.js
@@ -21,6 +21,34 @@ class UserController {
       return res.status(400).json({ error: error.message });
     }
   }
+
+  async update(req, res) {
+    try {
+      const { email, oldPassword } = req.body;
+
+      const user = await User.findByPk(req.userId);
+
+      if (email && email !== user.email) {
+        const userExists = await User.findOne({ where: { email } });
+
+        if (userExists) res.status(400).json({ error: 'User already exists.' });
+      }
+
+      if (oldPassword && !(await user.checkPassword(oldPassword)))
+        res.status(401).json({ error: 'Password does not match.' });
+
+      const { id, name, provider } = await user.update(req.body);
+
+      return res.json({
+        id,
+        name,
+        email,
+        provider,
+      });
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+  }
 }
 
 export default new UserController();
