611bd07616f44f33d0b954546a875b7900fea3d8
mpinheiro <mpinheiro@orion.tech>
2020-08-23 18:50:35 -0300

    feat send mail

---
+++ api/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'e085e74d1d4ddc',
+    pass: 'ad7b299ca7c212',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
